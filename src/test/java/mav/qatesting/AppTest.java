2cadfb280bcb61d3f2f6a45826ee40af1d334575
Julius Jackson <Julius.Jackson@realart.com>
2018-08-22 15:07:16 -0400

    Added ArrayList of Object and Assertion of li elements

--- src/test/java/mav/qatesting/AppTest.java
+++ src/test/java/mav/qatesting/AppTest.java
@@ -1,8 +1,12 @@
 package mav.qatesting;
-import org.junit.After;
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
+
 import commonfunctions.AbstractSteps;
 
 /**
@@ -11,21 +15,40 @@ import commonfunctions.AbstractSteps;
 public class AppTest extends AbstractSteps
 {
 	WebDriver driver = getDriver();
+	private int initial_counter;
 
 	@Before
 		public void openapp() throws Exception {
 			//Open the page
 			driver.navigate().to("http://localhost:4567/canvas_basic.html");
+			Canvas_Object.set_action_counter(driver);
+			initial_counter = Canvas_Object.get_action_counter();
 		}
 	
 	@Test
 		public void test1() throws Exception
 	    {
 			//Set X Y and Size Information
-			Canvas_Object.set_action_counter(driver);
-			Canvas_Object obj1 = new Canvas_Object(200,100,40, "Grey");
-			Canvas_Object.set_canvas(driver, obj1);
-//			Canvas_Object.clearoption(driver);
+			//Initial Count of Events
+			
+			
+			ArrayList<Canvas_Object> obj = new ArrayList<Canvas_Object>(); 
+			obj.add(new Canvas_Object());
+			obj.add(new Canvas_Object(200, 100, 40, "Grey"));
+			
+			Canvas_Object.set_canvas(driver, obj);
+			int addedevents = Canvas_Object.getAdded_events();
+			
+
+			int actual_counter = Canvas_Object.get_action_counter();
+			
+			
+			//Verifying the actual events are as expected
+			assertEquals(initial_counter + addedevents , actual_counter);
+			
+			
+			//Clears the options 
+			//Canvas_Object.clearoption(driver);
 	    }
 
 //	@After
--- src/test/java/mav/qatesting/Canvas_Object.java
+++ src/test/java/mav/qatesting/Canvas_Object.java
@@ -1,5 +1,6 @@
 package mav.qatesting;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.openqa.selenium.By;
@@ -13,10 +14,11 @@ public class Canvas_Object {
 	public int size = 0;
 	public String color;
 	private static int count;
+	private static int added_events = 0;
 	
 	public Canvas_Object(){
 		
-		x_cord = 20;
+		x_cord = 200;
 		y_cord = 200;
 		size = 40;
 		color = "Black";
@@ -28,23 +30,27 @@ public class Canvas_Object {
 		this.color = color;
 	}
 	
-	public static void set_canvas(WebDriver driver, Canvas_Object obj) {
+	public static void set_canvas(WebDriver driver, ArrayList<Canvas_Object> obj) {
 		//Clearing and Setting Values
-		
-		driver.findElement(By.id("xnum")).clear();
-		driver.findElement(By.id("xnum")).sendKeys(Integer.toString(obj.x_cord));
-		
-		driver.findElement(By.id("ynum")).clear();
-		driver.findElement(By.id("ynum")).sendKeys(Integer.toString(obj.y_cord));
-		
-		driver.findElement(By.id("shapesize")).clear();
-		driver.findElement(By.id("shapesize")).sendKeys(Integer.toString(obj.size));
-		
-		//DropDown
-		WebElement colourdropdown = driver.findElement(By.id("colourselect"));
-		Select dropdown = new Select(colourdropdown);
-		dropdown.selectByVisibleText(obj.color);
-		
+		for(int i = 0; i<obj.size();i++) {
+			driver.findElement(By.id("xnum")).clear();
+			driver.findElement(By.id("xnum")).sendKeys(Integer.toString(obj.get(i).x_cord));
+			
+			driver.findElement(By.id("ynum")).clear();
+			driver.findElement(By.id("ynum")).sendKeys(Integer.toString(obj.get(i).y_cord));
+			
+			driver.findElement(By.id("shapesize")).clear();
+			driver.findElement(By.id("shapesize")).sendKeys(Integer.toString(obj.get(i).size));
+			
+			//DropDown
+			WebElement colourdropdown = driver.findElement(By.id("colourselect"));
+			Select dropdown = new Select(colourdropdown);
+			dropdown.selectByVisibleText(obj.get(i).color);
+			
+			show(driver);
+			
+			
+		}
 		
 		
 		try {
@@ -56,11 +62,15 @@ public class Canvas_Object {
 	}
 	public static void clearoption(WebDriver driver) {
 		driver.findElement(By.name("clearbutton")).click();
+		setAdded_events();
+		set_action_counter(driver);
 		
 	}
 	
 	public static void show(WebDriver driver) {
 		driver.findElement(By.name("gobutton")).click();
+		setAdded_events();
+		set_action_counter(driver);
 	}
 	public void set_color(WebDriver driver) {
 	}
@@ -73,6 +83,12 @@ public class Canvas_Object {
 	}
 	public static int get_action_counter() {
 		return count;
+	}
+	public static int getAdded_events() {
+		return added_events;
+	}
+	public static void setAdded_events() {
+		Canvas_Object.added_events++;
 	}
 
 }
